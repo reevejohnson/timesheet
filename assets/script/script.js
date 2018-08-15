309463c5f3b1b26c224989abbcc2ee9bee3feea7
Reeve Johnson <reevewjohnson@gmail.com>
2018-08-14 20:16:42 -0500

    added unique IDs to rows

--- assets/script/script.js
+++ assets/script/script.js
@@ -44,10 +44,10 @@ $("#form-submit").on("click", function(event) {
       console.log(childSnapshot.val().rate);
 
 
-      $("#table-body").append("<tr><td id='logged-name'>" + childSnapshot.val().name +
-        "</td> <td id='logged-role'>" + childSnapshot.val().role +
-          "</td> <td id='logged-startDate'>" + childSnapshot.val().startDate +
-          "</td> <td> </td> <td id='logged-rate'>" + childSnapshot.val().rate + "</td> <td> </td> </tr>");
+      $("#table-body").append("<tr><td id='logged-name-" + childSnapshot.val().name + "'>" + childSnapshot.val().name +
+        "</td> <td id='logged-role-"+ childSnapshot.val().role +"'>" + childSnapshot.val().role +
+          "</td> <td id='logged-startDate-"+ childSnapshot.val().startDate +"'>" + childSnapshot.val().startDate +
+          "</td> <td> </td> <td id='logged-rate-"+ childSnapshot.val().rate +"'>" + childSnapshot.val().rate + "</td> <td> </td> </tr>");
     
   },
 
@@ -58,8 +58,8 @@ $("#form-submit").on("click", function(event) {
 
   database.ref().orderByChild("startDate").limitToLast(1).on("child_added", function(snapshot) {
     // Change the HTML to reflect
-    $("#logged-name").text(snapshot.val().name);
-    $("#logged-role").text(snapshot.val().role);
-    $("#logged-startDate").text(snapshot.val().startDate);
-    $("#logged-rate").text(snapshot.val().rate);
+    $("#logged-name-"+snapshot.val().name+"").text(snapshot.val().name);
+    $("#logged-role-"+snapshot.val().role+"").text(snapshot.val().role);
+    $("#logged-startDate-"+snapshot.val().startDate+"").text(snapshot.val().startDate);
+    $("#logged-rate-"+snapshot.val().rate+"").text(snapshot.val().rate);
   });
